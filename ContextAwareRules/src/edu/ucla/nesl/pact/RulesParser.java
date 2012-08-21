4664b2c5ff779c36a89640aef803563da36d879b
kastur <kastur@gmail.com>
2012-08-20 20:37:38 -0700

    Removed out/ directory.

---
+++ ContextAwareRules/src/edu/ucla/nesl/pact/RulesParser.java
@@ -0,0 +1,11 @@
+package edu.ucla.nesl.pact;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: k
+ * Date: 8/20/12
+ * Time: 7:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RulesParser {
+}
